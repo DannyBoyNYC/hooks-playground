438bd5850b832e25fa6c734b858f0db5362c981d
DannyBoyNYC <daniel.deverell@gmail.com>
2019-08-02 16:00:33 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,31 +1,59 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
-/*
-    1. The user can add new todo items
-    2. The user can remove todo items
-*/
-function Todo() {
-  const [todo, updateTodo] = useState('');
-  const [todos, updateTodos] = useState([]);
+import './styles.css';
+
+function useFetch(url) {
+  return {
+    loading: true,
+    data: null,
+    error: null
+  };
+}
+
+const postIds = [1, 2, 3, 4, 5, 6, 7, 8];
+
+function App() {
+  const [index, setIndex] = React.useState(0);
+
+  const { loading, data: post, error } = useFetch(
+    `https://jsonplaceholder.typicode.com/posts/${postIds[index]}`
+  );
+
+  const incrementIndex = () => {
+    setIndex(i => (i === postIds.length - 1 ? i : i + 1));
+  };
+
+  if (loading === true) {
+    return <p>Loading</p>;
+  }
+
+  if (error) {
+    return (
+      <React.Fragment>
+        <p>{error}</p>
+        <button onClick={incrementIndex}>Next Post</button>
+      </React.Fragment>
+    );
+  }
 
   return (
-    <div>
-      <label htmlFor="todo">
-        Todo
-        <input
-          type="text"
-          value={todo}
-          onChange={e => updateTodo(e.target.value)}
-        />
-      </label>
-      <button onClick={() => updateTodos(todo)}>Add Todo</button>
-      {todo}
-      {console.log(todos)}
-      {/* {todos != [] ? todos.map(todo => `<p>{todo}</p>`) : ''} */}
+    <div className='App'>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      {index === postIds.length - 1 ? (
+        <p>No more posts</p>
+      ) : (
+        <button onClick={incrementIndex}>Next Post</button>
+      )}
     </div>
   );
 }
 
-ReactDOM.render(<Todo />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
+
+/*
+  Instructions:
+    Implement the `useFetch` function. 
+*/
